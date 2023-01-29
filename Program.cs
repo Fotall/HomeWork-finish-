92633648e84ec90b034ffa6e6f30f5593582793a
Fotall <d.budarin@list.ru>
2023-01-29 17:32:08 +0300

    save 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+string[] Array = { "Hello", "2", "Hello", ":-)" };
+ 
+ int Limit = 3;
+ for (int i = 0; i < Array.Length;i++)
+     if(Array[i].Length <= Limit)
+         Console.WriteLine(Array[i]);
+ // Console .ReadLine();
+
+
+
